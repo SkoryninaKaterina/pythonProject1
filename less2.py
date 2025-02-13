3ccd7f1979408c267b5dd971895dd7792538e190
SkoryninaKaterina <skorynina.katerina@gmail.com>
2025-02-13 19:32:16 +0200

    dz2(11.02)

---
+++ less2.py
@@ -0,0 +1,71 @@
+# Завдання 1
+# Створіть клас Проект з атрибутами:
+#  назва
+#  виділений кошторис
+#  загальні витрати
+#  чи завершений(за замовчуванням False)
+#  час виконання(за замовчуванням 0 місяців)
+#  список необхідних задач
+# Додайте методи:
+#  вивід інформації: назва, час виконання, необхідні
+# задачі
+#  добавити нову задачу
+#  розбити задачу на під-задачі: передається назва задачі
+# та список під-задач
+#  виконати задачу, передається назва, час та ціна
+# виконання
+#  поповнення кошторису
+from distutils.sysconfig import project_base
+
+
+class Project:
+    def __init__(self, name, budget, tasks):
+        self.name = name
+        self.budget = budget
+        self.tasks = tasks
+
+        self.expenses = 0
+        self.is_finished = False
+        self.spent_time = 0  # місяці
+
+
+    def display_info(self):
+        print(f'Проект{self.name}')
+        print(f"Час виконання {self.spent_time} місяців")
+        print(f'витравчено {self.expenses}/{self.budget}$')
+        print(f'залишилось {self.budget - self.expenses}$')
+
+        if self.is_finished:
+            print('стан проекту завершений')
+        else:
+            print('стан проекту незавершений')
+
+
+            print('задачі що залишились:')
+            for task in self.tasks:
+                print(f'   - {task}')
+
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+
+
+project = Project(name='Ігрушка',
+                  budget=10_000,
+                  tasks=['знайти інвесторів',
+                         "придумати загальну ідею"])
+
+project.display_info()
+
+
+
+
+
+
+
+
+
+
+
+
